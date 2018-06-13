df623880ba223634e2361918db7d493c565826a5
Matt Mazzola <mattm@microsoft.com>
2018-06-13 15:20:20 -0700

    fix: add ChangeEntityResponse (#112)

---
+++ packages/cl-models/src/ChangeEntityResponse.ts
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+export interface ChangeEntityResponse {
+  packageId: string
+  entityId: string
+  negativeEntityId: string
+  trainingStatus: string
+}
