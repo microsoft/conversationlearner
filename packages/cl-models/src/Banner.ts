2050e08bb373986433a178d7bedab2f5ec7d761f
Lars Liden <larsliden@gmail.com>
2018-07-17 09:42:16 -0700

    feat: sdk version banner (#118)

--- packages/cl-models/src/Banner.ts
+++ packages/cl-models/src/Banner.ts
@@ -5,9 +5,11 @@
 
 /** Banner/Status info to display */
 export interface Banner {
-  message: string
-  type: string
-  datestring: string
-  link: string
-  linktext: string
+  message?: string
+  type?: string
+  datestring?: string
+  link?: string
+  linktext?: string
+  /* Only display message when SDK version is less that one listed below x.y.x */
+  sdkversion?: string
 }
