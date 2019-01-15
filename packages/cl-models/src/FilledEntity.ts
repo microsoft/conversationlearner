13e92f25559ed247a4634d20d4412bf5dccc7fd6
Lars Liden <larsliden@gmail.com>
2019-01-15 11:54:50 -0800

    fix: entity not displayed when missing (#159)
    * fix: entity not displayed when missing
    
    * fix: bad ref

--- packages/cl-models/src/FilledEntity.ts
+++ packages/cl-models/src/FilledEntity.ts
@@ -110,7 +110,7 @@ export class FilledEntityMap {
     }
 
     if (this.map[entityName].values.length === 0) {
-      return `[$entityName]`
+      return `[$${entityName}]`
     }
     // Print out[] list in friendly manner
     return filledEntityValueAsString(this.map[entityName])
