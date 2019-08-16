fbe383c86c084fb4944a8d5a27b51c2e30a6aa0d
Thomas Park <53840199+tpmsr@users.noreply.github.com>
2019-08-16 14:44:43 -0700

    fix: really really force release (#200)
    * style: kill semicolon
    
    * docs: comment

--- packages/cl-models/src/Callback.ts
+++ packages/cl-models/src/Callback.ts
@@ -2,8 +2,11 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
-/** Information about the running bot */
 
+/**
+ * Information about a callback action.
+ * logicArguments and renderArguments are only valid if the appropriate is_Provided field is true.
+ */
 export interface Callback {
   name: string
   logicArguments: string[]
