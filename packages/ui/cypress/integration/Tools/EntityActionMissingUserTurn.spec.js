39b72f017a503e74e364c3bd2026c47e811fe98c
Michael Skowronski <15230446+SDESkowronski@users.noreply.github.com>
2020-01-01 16:49:15 -0800

    fix: backup TestGrid and minor changes

--- packages/ui/cypress/integration/Tools/EntityActionMissingUserTurn.spec.js
+++ packages/ui/cypress/integration/Tools/EntityActionMissingUserTurn.spec.js
@@ -5,10 +5,9 @@
 
 import * as models from '../../support/Models'
 import * as modelPage from '../../support/components/ModelPage'
-import * as chatPanel from '../../../support/components/ChatPanel'
+import * as chatPanel from '../../support/components/ChatPanel'
 import * as train from '../../support/Train'
 import * as trainDialogsGrid from '../../support/components/TrainDialogsGrid'
-import * as common from '../../support/Common'
 import * as helpers from '../../support/Helpers'
 
 describe('Entity Action Missing User Turn', () => {
