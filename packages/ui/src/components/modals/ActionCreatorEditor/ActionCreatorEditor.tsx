3defeeea895816a368b09b4d817e4f3ef82f61d7
Matt Mazzola <mattm@microsoft.com>
2020-01-30 14:06:15 -0800

    chore: fix computation of action validity

--- packages/ui/src/components/modals/ActionCreatorEditor/ActionCreatorEditor.tsx
+++ packages/ui/src/components/modals/ActionCreatorEditor/ActionCreatorEditor.tsx
@@ -1600,11 +1600,12 @@ class ActionCreatorEditor extends React.Component<Props, ComponentState> {
                 return this.state.selectedCardOptionKey === undefined
             case CLM.ActionTypes.API_LOCAL:
                 return this.state.selectedApiOptionKey === undefined
-                    || (// If custom callback name is chose, must have name
-                        (this.state.selectedApiOptionKey === callbackNameInputOption.key
-                            && this.state.customCallbackName.length === 0)
-                        // Must have at least one mock result defined
-                        || this.state.callbackResults.length === 0)
+                    || // If custom callback name is chosen, must have name and mock results
+                    (this.state.selectedApiOptionKey === callbackNameInputOption.key
+                        && (this.state.customCallbackName.length === 0
+                            // Must have at least one mock result defined
+                            || this.state.callbackResults.length === 0)
+                    )
             case CLM.ActionTypes.SET_ENTITY:
                 return this.state.selectedEntityOptionKey === undefined
                     || this.state.selectedEnumValueOptionKey === undefined
