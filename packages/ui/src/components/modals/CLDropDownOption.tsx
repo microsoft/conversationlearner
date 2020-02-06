eb9000bb9ad089dbc39a5043d35ac6e3510e46ec
Matt Mazzola <mattm@microsoft.com>
2020-02-06 14:29:45 -0800

    chore: remove unused entity dropdown options

--- packages/ui/src/components/modals/CLDropDownOption.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) Microsoft Corporation. All rights reserved.  
- * Licensed under the MIT License.
- */
-import { IDropdownOption } from 'office-ui-fabric-react'
-
-export interface CLDropdownOption extends IDropdownOption {
-    style: string
-}
